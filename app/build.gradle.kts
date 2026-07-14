473f0d447cd26e0d7447dc6cef7fdaa9b71d1f81
kompoti <253134458+kompoti121@users.noreply.github.com>
2026-07-14 12:44:23 -0400

    Prepare 0.1.10 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.miruronative"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.1.10"
     }
 
     signingConfigs {
